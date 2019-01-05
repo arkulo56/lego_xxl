46739b668adb97d4fe2324d8dedca24f26c7aa07
arkulo56 <arkulo@126.com>
2019-01-05 18:00:16 +0800

    本期修改
    设置了底部导航栏

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,10 +1,12 @@
 //app.js
 App({
   globalData:{
-    openid:""
+    openid:"",
+    share:false,  //默认关闭分享
+    height:0  //头部导航栏高度
   },
-  onLaunch: function () {
-    
+  onLaunch: function (options) {
+    //开启用户追踪，获取用户信息
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -12,7 +14,19 @@ App({
         traceUser: true,
       })
     }
-
     this.globalData = {}
+
+    // 判断是否由分享进入小程序
+    if (options.scene == 1007 || options.scene == 1008) {
+      this.globalData.share = true
+    } else {
+      this.globalData.share = false
+    };
+
+    wx.getSystemInfo({
+      success: (res) => {
+        this.globalData.height = res.statusBarHeight
+      }
+    })
   }
 })
