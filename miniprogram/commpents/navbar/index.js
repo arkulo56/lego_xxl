a0bfff25fb87d97d7e499ccbb0850c02096ec9ad
arkulo56 <arkulo@126.com>
2019-01-31 16:17:53 +0800

    navbar简单修改
    把navbar的参数传递实现了一下

--- miniprogram/commpents/navbar/index.js
+++ miniprogram/commpents/navbar/index.js
@@ -4,14 +4,27 @@
  * 2. 第二个是页面内部转发功能，采用button绑定open-type='share'实现，但onShareAppMessage由
  * 调用页面自行实现
  * 3. 发送朋友圈功能直接跳转至自己实现的分享图片生成页面，下载海报成功后，会跳转回来
+ * 
+ * 
+ * Input:
+ * properties中定义了分享的原始页面和主键，知道原始页面地址，通过sharePages数组就能找到应该跳转去哪个分享页面
+ * 在调用方，wxml中：<navbar from-page="pages/note2/detail" page-id="{{detail._id}}"></navbar>
+ * 
  */
+
+import Dialog from '../../dist/dialog/dialog';
+
+var sharePages = {};
+sharePages["pages/note2/detail"] ="../page_share/index"
 
 const app = getApp()
 Component({
   properties: {
-    targetUrl:String,   //生成朋友圈海报地址，这里由外部调用页面直接将分享页面（包括参数id）传进来
+    fromPage:String,   //扫码海报跳转的原始页面
+    pageId:String,      //分享页面对应的主键
   },
   data: {
+    
   },
   attached: function () {
   },
@@ -24,11 +37,26 @@ Component({
     },
     //朋友圈海报
     _shareQuan(){
-      if (this.properties.targetUrl)
+      //console.log(this.properties.fromPage,":",this.properties.pageId)
+      var targetUrl = "";
+
+      if (this.properties.fromPage && this.properties.pageId)
       {
-        wx.navigateTo({
-          url: taregtUrl
-        })
+        if (sharePages.hasOwnProperty(this.properties.fromPage)) {
+          targetUrl = sharePages[this.properties.fromPage] + "?id=" + this.properties.pageId
+        } else {
+          console.log("没有找到指定的分享页面")
+        }
+
+
+        Dialog.alert({
+          title: '生成图片分享',
+          message: '分享图片将保存在您的相册中，请在朋友圈分享，谢谢！'
+        }).then(() => {
+          wx.navigateTo({
+            url: targetUrl,
+          })
+        });
       }
     }
   }
